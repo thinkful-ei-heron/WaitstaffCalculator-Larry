34cbfa43587128d98c40e4b41fb4cf1df751711c
larry <larryzie@gmail.com>
2019-09-25 19:14:25 -0500

    Added reset button handler

--- jscript/main.js
+++ jscript/main.js
@@ -1,2 +1,19 @@
-'use strict';
+/* eslint-disable no-console */
 
+function handlerReset() {
+  $('.mainWindow').on('click', '.allReset', function() {
+    initialize();
+  });
+}
+
+function initialize() {
+  //Clear all variables and render.
+  
+}
+
+function main() {
+  handlerReset();
+  initialize();
+}
+
+main ();
